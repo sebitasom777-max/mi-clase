98577a4fb71c5c28e7f2f3d391b1b301147f0199
sebitasom777-max <sebitasom777@gmail.com>
2026-09-20 20:36:17 -0500

    Add files via upload

---
+++ class OperadoresAritmeticos2 {.java
@@ -0,0 +1,13 @@
+class OperadoresAritmeticos2 {
+    public static void main(String[] args) {
+        double numero1 = 7.0;
+        double numero2 = 5.0;
+
+        System.out.println("Operaciones con \"numero1\" y \"numero2\":");
+        System.out.println(numero1 + numero2);  // Suma
+        System.out.println(numero1 - numero2);  // Resta
+        System.out.println(numero1 * numero2);  // Multiplicación
+        System.out.println(numero1 / numero2);  // División con decimales
+        System.out.println(numero1 % numero2);  // Módulo
+    } // Fin del método main
+} // Fin de la clase OperadoresAritmeticos 2
