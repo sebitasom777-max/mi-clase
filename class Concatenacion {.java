9c952ccf5d81a8fd8e295d19189f489ecbd22529
sebitasom777-max <sebitasom777@gmail.com>
2026-09-20 20:33:58 -0500

    Add files via upload

---
+++ class Concatenacion {.java
@@ -0,0 +1,8 @@
+class Concatenacion {
+    public static void main(String[] args) {
+        String nombre = "John";
+        String primerApellido = "Doe";
+        String nombreConApellido = nombre + " " + primerApellido;
+        System.out.println(nombreConApellido);
+    } // Fin del método main
+} // Fin de la clase Concatenacion
